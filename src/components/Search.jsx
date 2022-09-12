521268d50b85c69bba8a0a861b66e07412b4103c
yaser <ddemety372@gmail.com>
2022-09-12 14:13:27 +0300

    v1

---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div>
+        <div class="form-floating mb-3">
+  <input type="search" class="form-control" id="floatingInput" placeholder="name@example.com"/>
+  <label for="floatingInput">Email address</label>
+</div>
+    </div>
+  )
+}
+
+export default Search
